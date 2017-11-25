67fd29269484ff57222aaa740021f55887f69f86
No_otra_vez <saulbector@gmail.com>
2017-11-24 19:27:30 -0600

    Update title and Hs

--- js/countdown.js
+++ js/countdown.js
@@ -48,6 +48,6 @@ function startTimer(id, deadline){
 
 
 window.onload = function(){
-  var deadline = new Date("November 17, 2017 00:00:00");
+  var deadline = new Date("December 25, 2017 00:00:00");
   startTimer("clock", deadline);
 };
