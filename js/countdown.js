14199d959d30b784cbda8ff393852639813f4c9e
No_otra_vez <saulbector@gmail.com>
2020-12-24 00:09:14 -0600

    Cambio de fecha

--- js/countdown.js
+++ js/countdown.js
@@ -48,6 +48,6 @@ function startTimer(id, deadline){
 
 
 window.onload = function(){
-  var deadline = new Date("January 1, 2021 00:00:00");
+  var deadline = new Date("December 31, 2020 00:00:00");
   startTimer("clock", deadline);
 };
