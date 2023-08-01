237794bf59e8cc2e02e5a6aca0058ad0331046c2
No_otra_vez <saulbector@gmail.com>
2023-08-01 17:21:52 -0600

    R3F Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Canvas } from '@react-three/fiber'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [0, 0, 2] }}>
+        <mesh>
+          <boxGeometry />
+          <meshBasicMaterial color={0x00ff00} wireframe />
+        </mesh>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
