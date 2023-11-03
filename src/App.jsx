1a24f230f8cf956b013e654acc270c2ed51320a5
No_otra_vez <saulbector@gmail.com>
2023-11-02 19:00:21 -0600

    Change h2 in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
       <Scene />
       <div className="mensaje">
         <h1>sbector.com</h1>
-        <h2>en mantenimiento</h2>
       </div>
       <div className="portafolio">
         <a href="./Saúl Becerra Torres_ Portafolio 2023.pdf">Portafolio 2023</a>
